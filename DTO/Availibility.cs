97c39b68377cdadca6520595b7703257b05085db
alex.gharbi@hotmail.com <alex.gharbi@hotmail.com>
2019-10-28 16:26:53 +0100

    DTO and few objects

---
+++ DTO/Availibility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    class Availibility
+    {
+        public int idAvailability { get; set; }
+        
+        public Boolean isAvailable { get; set; }
+
+        public DateTime time { get; set; }
+
+        public int idStaff { get; set; }
+
+
+    }
+}
---
+++ DTO/cities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    class cities
+    {
+        public int idCity { get; set; }
+        public string city { get; set; }
+        public int post_code { get; set; }
+
+
+    }
+}
